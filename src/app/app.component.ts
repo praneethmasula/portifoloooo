ba383c0b23e42377ce709103dac052fc18a19847
praneethmasula <praneethmasula24@gmail.com>
2023-12-09 19:10:55 +0530

    pro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,18 @@ import { Component, HostListener } from '@angular/core';
 export class AppComponent {
   title = 'portifolio-fronend';
   isSticky: boolean = false;
+  ele: any;
 
   @HostListener('window:scroll', ['$event'])
   checkScroll() {
     this.isSticky = window.pageYOffset >= 250;
+  }
+  scroll(s:string){
+     this.ele=document.getElementById(s);
+    this.ele.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
   }
 }
